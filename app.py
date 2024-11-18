7bc7b8a4cf4f0f2361a2f0d1d4d9bacc888cd9c2
chen1829 <chenhoward413@gmail.com>
2024-11-18 07:51:57 +0000

    111

--- app.py
+++ app.py
@@ -2,9 +2,9 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/", methods=["GET", "PUT"])
+@app.route("/", methods=["GET", "Post"])
 def index():
-    return render_template("index.html")
+    return(render_template("index.html"))
 
 if __name__ == "__main__":
-    app.run()
+    app.run()
